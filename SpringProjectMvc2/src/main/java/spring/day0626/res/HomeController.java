1713e87127fa7fe24720d94f88a261ba022127e1
hhh <gks0405@koreatech.ac.kr>
2020-06-27 17:23:49 +0900

    0627_mvc2_start

--- SpringProjectMvc2/src/main/java/spring/day0626/res/HomeController.java
+++ SpringProjectMvc2/src/main/java/spring/day0626/res/HomeController.java
@@ -23,17 +23,12 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public String home(Model model) {
+	
+		model.addAttribute("myimg1","image/yona.png");
+		model.addAttribute("myimg2","image/yeonhee.png");
+		model.addAttribute("title","좋아하는 연애인");
+		return "shop/list";
 	}
 	
 }
